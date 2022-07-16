ead6c305ff5998d82e4d23e6cad5cbf773305857
Ashwin Narayan <ashwinnarayan1994@gmail.com>
2022-07-16 13:05:57 +0800

    Fixed some small bugs.

--- src/components/NFTMaker.tsx
+++ src/components/NFTMaker.tsx
@@ -148,11 +148,6 @@ export default function NFTMaker({}: Props) {
         <Row>
         <Toast show={isMinting} animation={false}>
           <Toast.Header>
-            <img
-              src="holder.js/20x20?text=%20"
-              className="rounded me-2"
-              alt=""
-            />
             <strong className="me-auto">Bootstrap</strong>
             <small>11 mins ago</small>
           </Toast.Header>
